970973cdbd0c87850bd40677f6b2e026de46cd22
yang.tianyu <yang.tianyu@sh.adways.net>
2014-08-18 17:08:21 +0800

    change http actions to multithread

--- cap.py
+++ cap.py
@@ -1,33 +1,37 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import Image, pytesseract
-import urllib2, cookielib,Cookie
+import Image
+import urllib2, cookielib
+import gzip, string, random, os, time, sys
+import threading
 from StringIO import StringIO
-import gzip, re, string, random, os, time
 
+count = 0
 imgWidth = 15
 imgHeight = 26
 imageDic = dict()
-cj = None
-path = '/Users/yangtianyu/Develop/captcha/sample/'
-header =  {'Host':'www.7do.net',
+path = './sample/'
+table = [i / 254 for i in range(256)]
+lock = threading.RLock()
+log = open("log.txt", 'w')
+
+commonHeader =  {'Host':'www.7do.net',
 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36',
 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 'Accept-Encoding':'gzip,deflate,sdch',
 'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
 }
 
-table = []
-for i in range(256):
-    if  i < 254:
-        table.append(0)
-    else:
-        table.append(1)
+def initSamples():
+    for parent,dirnames,filenames in os.walk(path): 
+        for filename in filenames:
+            if filename[:1] != '.':
+                img = Image.open(path + filename)
+                imageDic[filename[:-4]] = img.load()
 
 def compareImg(imgData):
     minimal = imgHeight * imgWidth
-    key = ''
     for k, v in imageDic.iteritems():
         difference = 0
         for y in xrange(imgHeight):
@@ -39,52 +43,22 @@ def compareImg(imgData):
             key = k[:1]
     return key
 
-def initSamples():
-    for parent,dirnames,filenames in os.walk(path): 
-        for filename in filenames:
-            if filename[:1] != '.':
-                img = Image.open(path + filename)
-                imageDic[filename[:-4]] = img.load()
-
-def initWeb():
-    global cj
-    cj = cookielib.LWPCookieJar('cookie.txt')
-    cj.load('cookieTemplate.txt')
-    cj.save(ignore_discard=True, ignore_expires=True)
-
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    urllib2.install_opener(opener)
-    header['X-Forwarded-For'] = '{0}.{1}.{2}.{3}'.format(random.randrange(255), random.randrange(255), random.randrange(255), random.randrange(255))
-
-def req(header, data, url, unzip = True, specail = False):
-    request = urllib2.Request(url = url,data = data, headers = header)
-    response = urllib2.urlopen(request)
-    headers = str(response.headers.headers)
-    if specail:
-        print(headers)
-    rawdata = response.read()
-    cj.save(ignore_discard = True, ignore_expires = True)
-    if specail:
-        if str(headers).find('creditnotice') > 0:
-                print("**********************success**************************")
-
-    if not unzip:
-        return rawdata
-    return gzip.GzipFile(fileobj = StringIO(rawdata)).read()
-
 def getSubStr(str, prefix, sufix, begin = 0):
     start = str.find(prefix, begin)
     pos = start + len(prefix)
     return str[pos : str.find(sufix, pos)], pos
 
 def randomStr():
-    letters=string.ascii_letters
+    letters = string.ascii_letters
     return ''.join([random.choice(letters) for _ in range(5)])
 
 def processImage(data, img):
     if not img:
         img = Image.open(StringIO(data))
     img = img.convert("RGB")
+    # global imgName
+    # img.save(str(imgName) + '.png')
+    # imgName = imgName + 1
     # img.save('dl.png')
     pixdata = img.load()
 
@@ -113,7 +87,10 @@ def processImage(data, img):
             if allWhite:
                 findBlack = not findBlack
                 px.append(x)
-    # print(px)
+    if len(px) == 7:
+        px.append(img.size[0] - 1)
+
+    # log.write(str(px) + '\n')
 
     py = []
     for i in xrange(len(px) / 2):
@@ -136,7 +113,7 @@ def processImage(data, img):
                     break
                 elif j == img.size[1] - 1:
                     py.append(j) 
-    # print(py)
+    # log.write(str(py) + '\n')
     
     ret = ''
     for i in xrange(4):
@@ -150,80 +127,85 @@ def processImage(data, img):
     
     return ret
 
-def doRequests():
+class register(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self, name = name)
+        self.cj = cookielib.LWPCookieJar(name)
+        self.header = commonHeader.copy()
 
-    req(header, None, 'http://www.7do.net/?fromuser=Tdfh')
-    
-    header["Referer"] = 'http://www.7do.net/?fromuser=Tdfh'
-    req(header, None, 'http://www.7do.net/home.php?mod=misc&ac=sendmail&rand=' + str(int(time.time())))
+    def initWeb(self):
+        self.cj.revert('cookieTemplate.txt')
+        self.cj.save(ignore_discard = True, ignore_expires = True)
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        self.header['X-Forwarded-For'] = '{0}.{1}.{2}.{3}'.format(random.randrange(255), random.randrange(255), random.randrange(255), random.randrange(255))
 
-    copyHeader = header.copy()
-    req(copyHeader , None, 'http://www.7do.net/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login')
 
-    webContent = req(header, None, 'http://www.7do.net/member.php?mod=register')
+    def req(self, url, unzip = True, data = None, specail = False):
+        request = urllib2.Request(url = url, data = data, headers = self.header)
+        response = self.opener.open(request)
+        headers = str(response.headers.headers)
+        # if specail:
+            # log.write(headers + '\n')
+        rawdata = response.read()
+        self.cj.save(ignore_discard = True, ignore_expires = True)
+        if specail and str(headers).find('creditnotice') > 0:
+            lock.acquire()
+            log.write("**********************success**************************\n")
+            lock.release()
 
-    hashid = getSubStr(webContent, 'sechash" type="hidden" value="', '"')[0]
+        if not unzip:
+            return rawdata
+        return gzip.GzipFile(fileobj = StringIO(rawdata)).read()
 
-    formhash = getSubStr(webContent, 'formhash" value="', '"')[0]
+    def doRequests(self):
+        self.req('http://www.7do.net/?fromuser=Tdfh')
+        
+        self.header["Referer"] = 'http://www.7do.net/?fromuser=Tdfh'
+        self.req('http://www.7do.net/home.php?mod=misc&ac=sendmail&rand=' + str(int(time.time())))
+        self.req('http://www.7do.net/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login')
+        webContent = self.req('http://www.7do.net/member.php?mod=register')
 
-    name, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"')
-    password, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"', begin)
-    passwordConfirm, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"', begin)
-    email, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"', begin)
+        hashid = getSubStr(webContent, 'sechash" type="hidden" value="', '"')[0]
+        name, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"')
+        password, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"', begin)
+        passwordConfirm, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"', begin)
+        email, begin = getSubStr(webContent, '<th><span class="rq">*</span><label for="', '"', begin)
 
-    header['Referer'] = 'http://www.7do.net/member.php?mod=register'
-    req(header, None, 'http://www.7do.net/home.php?mod=misc&ac=sendmail&rand=' + str(int(time.time())))
+        self.header['Referer'] = 'http://www.7do.net/member.php?mod=register'
+        self.req('http://www.7do.net/home.php?mod=misc&ac=sendmail&rand=' + str(int(time.time())))
 
-    cap = ''
-    while True:
-        url = 'http://www.7do.net/misc.php?mod=seccode&action=update&idhash={0}&inajax=1&ajaxtarget=seccode_{1}'.format(hashid, hashid)
-        content = req(header, None, url)
+        while True:
+            url = 'http://www.7do.net/misc.php?mod=seccode&action=update&idhash={0}&inajax=1&ajaxtarget=seccode_{1}'.format(hashid, hashid)
+            content = self.req(url)
 
-        pa = 'src="'
-        params = getSubStr(content, pa, '"')[0]
+            pa = 'src="'
+            params = getSubStr(content, pa, '"')[0]
 
-        url = 'http://www.7do.net/' + params
-        captcha = req(header, None, url, False)
+            url = 'http://www.7do.net/' + params
+            captcha = self.req(url, False)
 
-        cap = processImage(captcha, None)
-        print(cap)
+            cap = processImage(captcha, None)
+            log.write(cap + '\n')
 
-        url = 'http://www.7do.net/misc.php?mod=seccode&action=check&inajax=1&&idhash='+ hashid + '&secverify=' + cap
-        answer = req(copyHeader, None, url)
-        print(answer)
-        if answer.find('invalid') <= 0:
-            break
+            self.header['X-Requested-With'] = 'XMLHttpRequest'
+            url = 'http://www.7do.net/misc.php?mod=seccode&action=check&inajax=1&&idhash='+ hashid + '&secverify=' + cap
+            answer = self.req(url)
+            del self.header['X-Requested-With']
 
-    username = randomStr()
-    userEmail = randomStr() + '@126.com'
-    copyHeader = header.copy()
-    copyHeader['X-Requested-With'] = 'XMLHttpRequest'
-    url = 'http://www.7do.net/forum.php?mod=ajax&inajax=yes&infloat=register&handlekey=register&ajaxmenu=1&action=checkusername&username=' + username
-    req(copyHeader, None, url)
+            lock.acquire()
+            log.write(answer + '\n')
+            lock.release()
 
-    url = 'http://www.7do.net/forum.php?mod=ajax&inajax=yes&infloat=register&handlekey=register&ajaxmenu=1&action=checkemail&email=' + userEmail
-    req(copyHeader, None, url)
+            if answer.find('invalid') <= 0:
+                lock.acquire()
+                log.write('valid')
+                lock.release()
+                break
 
-    url = 'http://www.7do.net/misc.php?mod=seccode&action=check&inajax=1&&idhash='+ hashid + '&secverify=' + cap
-    answer = req(copyHeader, None, url)
+        username = randomStr()
+        userEmail = randomStr() + random.choice(('@126.com', '@qq.com', '@163.com', '@gmail.com', '@hotmail.com'))
 
-    while answer.find('invalid') > 0:
-        url = 'http://www.7do.net/misc.php?mod=seccode&action=update&idhash={0}&inajax=1&ajaxtarget=seccode_{1}'.format(hashid, hashid)
-    
-        content = req(header, None, url)
-
-        pa = 'src="'
-        params = getSubStr(content, pa, '"')[0]
-
-        url = 'http://www.7do.net/' + params
-        # print(url)
-        captcha = req(header, None, url, False)
-
-        cap = processImage(captcha, None)
-        url = 'http://www.7do.net/misc.php?mod=seccode&action=check&inajax=1&&idhash='+ hashid + '&secverify=' + cap
-        answer = req(copyHeader, None, url)
-    print('valid')
-    data = '''------WebKitFormBoundary3untao1xYPCyrRO1\r
+        data = '''------WebKitFormBoundary3untao1xYPCyrRO1\r
 Content-Disposition: form-data; name="regsubmit"\r
 \r
 yes\r
@@ -268,20 +250,36 @@ Content-Disposition: form-data; name="regsubmit"\r
 \r
 true\r
 ------WebKitFormBoundary3untao1xYPCyrRO1--'''.format(name, username, password, passwordConfirm, email, userEmail, hashid, cap)
-    # print(data)
+        # print(data)
 
-    header['Content-Type'] = 'multipart/form-data; boundary=----WebKitFormBoundary3untao1xYPCyrRO1'
+        self.header['Content-Type'] = 'multipart/form-data; boundary=----WebKitFormBoundary3untao1xYPCyrRO1'
+        self.req('http://www.7do.net/member.php?mod=register&inajax=1', True, data, True)
+        del self.header['Content-Type']
 
-    req(header, data, 'http://www.7do.net/member.php?mod=register&inajax=1', True, True)
+    def run(self):
+        global count
+        while True:
+            lock.acquire()
+            if count == 0:
+                lock.release()
+                break
+            print('count is', count)
+            count = count - 1
+            lock.release()
+            
+            self.initWeb()
+            self.doRequests()
+        os.remove(self.cj.filename)
+        print(self.getName() + ' finished')
 
+if __name__ == '__main__':
+    if len(sys.argv) != 2 :
+        print("Enter a count")
+        exit()
+    count = int(sys.argv[1])
+    initSamples()
 
-initSamples()
-
-times = 0
-while times < 1:
-    initWeb()
-    doRequests()
-    times = times + 1
-
-
-# print(processImage(None, Image.open('misc.png')))#/Users/yangtianyu/Desktop/captcha.png')))
+    threadCount = min(count, 10)
+    for x in xrange(threadCount):
+        thread = register(str(x))
+        thread.start()
